f9cbb795de98a5c83d40e2b94984782842ca2330
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2018-05-22 14:43:34 +0800

    database verbs list

--- app/src/main/java/armorhin/ept003/VerbActivity.java
+++ app/src/main/java/armorhin/ept003/VerbActivity.java
@@ -9,21 +9,56 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.io.IOException;
+
 
 public class VerbActivity extends AppCompatActivity {
 
-        ListView listVerb;
-        SimpleCursorAdapter scAdapter;
-        Cursor c;
+    ListView listVerb;
+    SimpleCursorAdapter scAdapter;
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
 
-         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_verb);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verb);
 
-            listVerb = (ListView) findViewById(R.id.listVerb);
+        mDBHelper = new DatabaseHelper(this);
 
-
+        // обновление базы данных
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException e) {
+            throw new RuntimeException("Невозможно загрузить базу данных", e);
         }
+
+        mDb = mDBHelper.getWritableDatabase();
+
+
+        //виджеты в плашке для отдельного элемента списка
+        int[] fields = new int[]{R.id.txtInfinitive, R.id.txtPastSimple, R.id.txtPastParticiple, R.id.txtTranslation};
+
+        // заголовки столбцов результирующей таблицы-выборки из базы данных
+        String[] headers = new String[]{"infinitive", "pastsimple", "pastparticiple", "translation"};
+
+        // сделаем выборку данных из базы данных, указав необходимые столбцы и таблицу
+        // последний параметр null означает, что мы не делаем фильтрацию, выбирам все строки таблицы БД.
+        Cursor c = mDb.rawQuery("SELECT id AS _id, infinitive, pastsimple, pastparticiple, translation FROM verbs", null);
+
+        // на базе курсора создаем адаптер, используя все заготовленные параметры и данные.
+        scAdapter = new SimpleCursorAdapter(this, R.layout.verb_list_item, c, headers, fields, SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        listVerb = (ListView) findViewById(R.id.listVerb);
+        listVerb.setAdapter(scAdapter);
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Закрываем соединение с базой данных
+        mDBHelper.close();
+    }
+}
