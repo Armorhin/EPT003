a8680023c3b4a4371aac981b8ec0bc21951cac25
Armorhin <mr.doc1101@yandex.ru>
2018-05-24 11:00:57 +0800

    01

--- app/src/main/java/armorhin/ept003/PracticeActivity.java
+++ app/src/main/java/armorhin/ept003/PracticeActivity.java
@@ -1,13 +1,21 @@
 package armorhin.ept003;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class PracticeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
     }
 }
